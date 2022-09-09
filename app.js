fe61c3170a8ae91eb1f8d1869a00314f0c0d6cff
Victor Okechukwu <VicOke360@gmail.com>
2022-09-09 05:21:09 +0100

    chore: setTimeout.js added!

---
+++ app.js
@@ -0,0 +1,1 @@
+require('./eventLoop/1-SetTimeout')
--- eventLoop/1-SetTimeout.js
+++ eventLoop/1-SetTimeout.js
@@ -1,3 +1,11 @@
-// setTimeout(() => {
-//     console.log('Hello world')
-// }, 3);
+setTimeout(() => {
+    console.log('Hello Victor')
+}, 5000);
+// console.log(new Date(Date.now()));
+// console.log(new Date(Date.UTC(22, 9, 9, 0, 0, 0)))
+// console.log(Date)
+// console.log(Date)
+// const today = new Date();
+// const year = today.getFullYear(2022);
+// console.log(year)
+// console.log(today.getMonth()+1)
