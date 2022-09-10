a53531025b181f32198cc64b54b79ea4cceb5150
Victor Okechukwu <VicOke360@gmail.com>
2022-09-11 00:56:04 +0100

    added await.pattern.js

--- app.js
+++ app.js
@@ -2,5 +2,6 @@
 // require('./eventLoop/server')
 // require('./8-fsSync')
 // require('./8-fsAsync')
-require('./2-async-patterns.js/blocks')
+// require('./2-async-patterns.js/blocks')
+require('./2-async-patterns.js/await.pattern')
 
