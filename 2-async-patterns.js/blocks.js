cdb3b0e4a84306a3859251a1d49de6a660d311d9
Victor Okechukwu <VicOke360@gmail.com>
2022-09-11 00:45:23 +0100

    added blocks.js

---
+++ 2-async-patterns.js/blocks.js
@@ -0,0 +1,25 @@
+const http = require("http")
+
+const serrver = http.createServer((req, res) => {
+    if (req.url === '/') {
+        console.log("We're in the Home page")
+        res.end("We're in the Home page")
+    }
+    if (req.url === '/about') {
+        //BLOCKING CODE
+        for(i = 0; i < 60; i++) {
+            for(j = 0; j < 60; j++) {
+                console.log(`${i}, ${j}`)
+            }
+        }
+        console.log("We're in the About page")
+        res.end("We're in the About page")
+    }
+    // res.end("Page not found")
+})
+
+
+const PORT = 3333
+serrver.listen(PORT, () => {
+    console.log(`Server is listening on ${PORT}`)
+})
--- app.js
+++ app.js
@@ -2,29 +2,5 @@
 // require('./eventLoop/server')
 // require('./8-fsSync')
 // require('./8-fsAsync')
+require('./2-async-patterns.js/blocks')
 
-const http = require("http")
-
-const serrver = http.createServer((req, res) => {
-    if (req.url === '/') {
-        console.log("We're in the Home page")
-        res.end("We're in the Home page")
-    }
-    if (req.url === '/about') {
-        //BLOCKING CODE
-        for(i = 0; i < 60; i++) {
-            for(j = 0; j < 60; j++) {
-                console.log(`${i}, ${j}`)
-            }
-        }
-        console.log("We're in the About page")
-        res.end("We're in the About page")
-    }
-    // res.end("Page not found")
-})
-
-
-const PORT = 3333
-serrver.listen(PORT, () => {
-    console.log(`Server is listening on ${PORT}`)
-})
