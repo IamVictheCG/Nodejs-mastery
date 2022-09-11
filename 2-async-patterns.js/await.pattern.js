c1d3a613816a6ee7e078c16843ce7d4cf9716362
Victor Okechukwu <VicOke360@gmail.com>
2022-09-11 01:35:23 +0100

    chore: updated await.pattern.js

--- 2-async-patterns.js/await.pattern.js
+++ 2-async-patterns.js/await.pattern.js
@@ -7,11 +7,11 @@ const getTask = (path) => {
     return new Promise((resolve, reject) => {
         readFile(path, "utf8", ((err, data) => {
             if (err) {
-                console.log(err)
+                // console.log(err)
                 reject(err)
                 return
             } else {
-                console.log(data)
+                // console.log(data)
                 resolve(data)
             }
         }
@@ -19,6 +19,13 @@ const getTask = (path) => {
     })
 }
 
-getTask('./content/first.txt')
+
+
+const start = async () => {
+    getTask("./content/first.txt")
+}
+start()
+
+getTask("./content/first.txt")
 .then((result) => console.log(result))
 .catch((err) => console.log(err, "Something has gone wrong"))
