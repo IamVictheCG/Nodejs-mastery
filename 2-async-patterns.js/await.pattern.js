6e6a17a2cd3cb9c07cc19d38dc3ba4a46eb515d9
Victor Okechukwu <VicOke360@gmail.com>
2022-09-11 01:15:46 +0100

    chore: updated await.pattern.js

--- 2-async-patterns.js/await.pattern.js
+++ 2-async-patterns.js/await.pattern.js
@@ -1,11 +1,24 @@
 const {readFile} = require('fs')
+const { reject, get } = require('lodash')
+const { resolve } = require('path')
+const path = require('path')
 
-readFile("./content/first.txt", "utf8", ((err, data) => {
-    if (err) {
-        console.log(err)
-        return
-    } else {
-        console.log(dat)
-    }
+const getTask = (path) => {
+    return new Promise((resolve, reject) => {
+        readFile(path, "utf8", ((err, data) => {
+            if (err) {
+                console.log(err)
+                reject(err)
+                return
+            } else {
+                console.log(data)
+                resolve(data)
+            }
+        }
+        ))
+    })
 }
-))
+
+getTask('./content/first.txt')
+.then((result) => console.log(result))
+.catch((err) => console.log(err, "Something has gone wrong"))
