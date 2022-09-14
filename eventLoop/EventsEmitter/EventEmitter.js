871e3dc36b2caab952a89fe2198a5ae8d8e5473f
Victor Okechukwu <VicOke360@gmail.com>
2022-09-14 01:53:14 +0100

    updated EventEmitter.js

--- eventLoop/EventsEmitter/EventEmitter.js
+++ eventLoop/EventsEmitter/EventEmitter.js
@@ -4,42 +4,16 @@ const EventEmitter = require('events')
 
 const customEmitter = new EventEmitter
 
-// customEmitter.on('response', (name, id) => {
-//     console.log(`Data recieved; User: ${name} Id: ${id}`)
-// })
-// customEmitter.on('response', (age, gender) => {
-//     // console.log(`Data recieved; User: ${name} Id: ${id}`)
-//     console.log(`Analysing data; Age: ${age} Gender: ${gender}`)
-// })
-// customEmitter.on('response1', (age, gender) => {
-//     console.log(`Analysing data; Age: ${age} Gender: ${gender}`)
-// })
-
-
-
-// customEmitter.emit("response", 'John', 22)
-// customEmitter.emit("response1", 22, "Male")
-
-
-
-////
-
 // function eemit(name, id,) {
     customEmitter.on('response', (name, id, age, gender) => {
         console.log(`Data recieved; User: ${name} Id: ${id}`)
     })
-    // customEmitter.on('response', () => {
-    //     // console.log(`Data recieved; User: ${name} Id: ${id}`)
-    //     console.log("Analysing data")
-    // })
+
     customEmitter.on('response', (name, id, age, gender) => {
         console.log(`Analysing data; Age: ${age} Gender: ${gender}`)
     })
-    // customEmitter.on('response1', (age, gender) => {
-    //     console.log(`Analysing data; Age: ${age} Gender: ${gender}`)
-    // })
     
-    customEmitter.emit("response", 'John', 22, 24, "Male")
+    customEmitter.emit("response", 'John', "J22", "24", "Male")
 // }
 
 // eemit()
