688d7b8d350e9d5d6c914efc898572f25f0f2776
Victor Okechukwu <VicOke360@gmail.com>
2022-09-16 11:22:27 +0100

    13-CreateReadStreams updated

--- 14-CreateReadStream.js
+++ 14-CreateReadStream.js
@@ -3,7 +3,9 @@ const {createReadStream} = require("fs")
 const stream = createReadStream('./content/bigFile.txt')
 
 //default 64kb
-//last buffer
+//last buffer - remainder
+//highwatermark -  controls the size
+//utf8 
 
 stream.on('data', (result) => {
     console.log(result)
