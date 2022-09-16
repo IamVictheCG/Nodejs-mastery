fa833a401a985c5d4dff97e4dda6ae198582d76c
Victor Okechukwu <VicOke360@gmail.com>
2022-09-16 02:46:05 +0100

    13-Create-Big-File.js updated

--- 13-Create-Big-File.js
+++ 13-Create-Big-File.js
@@ -13,7 +13,7 @@ server.listen(5223)
 
 
 //This creates a large file returning "Hello World" for an iteration of 100 times
-// It
+// It returns
 for (i = 0; i <= 100; i++) {
     writeFileSync("./content/bigFile.txt", `Hello World --- ${i}\n`, {flag: 'a'})
 } return
