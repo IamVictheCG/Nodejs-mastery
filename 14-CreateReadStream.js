30c68abd98bc88fdcb79287698e1d92e392b891b
Victor Okechukwu <VicOke360@gmail.com>
2022-09-16 11:24:33 +0100

    13-CreateReadStreams updated

--- 14-CreateReadStream.js
+++ 14-CreateReadStream.js
@@ -5,7 +5,9 @@ const stream = createReadStream('./content/bigFile.txt')
 //default 64kb
 //last buffer - remainder
 //highwatermark -  controls the size
-//utf8 
+//utf8 - sets the encoding
+// const stream = createReadStream('./content/bigFile.txt', {highWaterMark: 9000})
+// const stream = createReadStream('./content/bigFile.txt', {encoding: "utf8"})
 
 stream.on('data', (result) => {
     console.log(result)
