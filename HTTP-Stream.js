96306cfe53706b9651e3f71b8f9e4076e3934c9c
Victor Okechukwu <VicOke360@gmail.com>
2022-09-17 10:32:18 +0100

    made changes

--- HTTP-Stream.js
+++ HTTP-Stream.js
@@ -5,10 +5,23 @@ const fs = require('fs')
 
 const server = http.createServer()
 
+// server.on("request", (req, res) => {
+//     console.log("Server is up and running")
+//     const largeText = fs.readFileSync("./content/bigFile.txt",  "utf8")
+//     res.end(`Welcome!\n\n\n\n${largeText}`)
+// })
+
+//or
+
 server.on("request", (req, res) => {
     console.log("Server is up and running")
-    const largeText = fs.readFileSync("./content/bigFile.txt",  "utf8")
-    res.end(`Welcome!\n\n\n\n${largeText}`)
+    const fileStream = fs.createReadStream("./content/bigFile.txt",  "utf8")
+    fileStream.on('open', () => {
+        fileStream.pipe(res)
+    })
+    fileStream.on('error', (err) => {
+        res.end(err)
+    })
 })
 
 server.listen(5220)
