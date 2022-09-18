59f3dc12122b7990c08dd7ae02f360a9982d4a60
Victor Okechukwu <VicOke360@gmail.com>
2022-09-18 08:22:00 +0100

    changes made

--- app.js
+++ app.js
@@ -8,25 +8,29 @@
 // require("./12-Streams")
 // require('./14-CreateReadStream')
 // require("./HTTP-Stream")
-
+// require('./2-Express-Tutorial/navbar-app')
 const http = require('http');
+const { readFileSync } = require('fs')
 
+const homePage = readFileSync('./2-Express-Tutorial/navbar-app/index.html')
 const server = http.createServer((req, res) => {
     const url = req.url
     if (url === "/") {
         console.log("Server is runing, Home Page")
         res.writeHead(200, {'contet-type':'text/html'})
-        res.write('<h1>Home Page</h1>')
+        res.write(homePage)
         res.end("Good day to you User")
-    } else if(url === '/ about') {
+    } else if(url === '/about') {
         console.log("Server is runing, About Page")
         res.writeHead(200, {'contet-type':'text/html'})
         res.write('<h1>About Page</h1>')
         res.end("Welcome to the about page!!!")
     }
-    res.writeHead(404, {'contet-type':'text/html'})
-    res.write('<h1>Page not found</h1>')
-    res.end("Try again")
+    else {
+        res.writeHead(404, {'contet-type':'text/html'})
+        res.write('<h1>Page not found</h1>')
+        res.end("Try again")
+    }
 })
 
 // const Port = 5011
