57cede5b98d0fe100407aa81c0d5304a4c331fff
Victor Okechukwu <VicOke360@gmail.com>
2022-09-18 23:36:45 +0100

    chore: Updated

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
     console.log(`About page accessed`)
-//     res.status(200).send(`You are now at the about page`)
+    res.status(200).send(`You are now at the about page`)
 // })
 
 // app.all('*', (req, res) => {
