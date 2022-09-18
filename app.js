2e577b3701cca5897f153b149b1cfe18c9b7be3b
Victor Okechukwu <VicOke360@gmail.com>
2022-09-18 23:35:43 +0100

    chore: Updated

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 const express = require('express')
 const app = express()
 
-const PORT = 5011
-
-app.listen(PORT,() => {
-    console.log(`Server is listening on port ${PORT}`)
-})
-
 // app.get
 // app.post
 // app.put
@@ -14,3 +8,23 @@ app.listen(PORT,() => {
 // app.use
 // app.all
 // app.listen
+
+const PORT = 5011
+
+app.listen(PORT,() => {
+    console.log(`Server is listening on port ${PORT}`)
+})
+
+app.get('/', (req, res) => {
+    console.log(`Homepage accessed`)
+    res.status(200).send(`You are now at the home page`)
+})
+
+// app.get('/about', (req, res) => {
+//     console.log(`About page accessed`)
+//     res.status(200).send(`You are now at the about page`)
+// })
+
+// app.all('*', (req, res) => {
+//     res.status(404).send(`Resourece page not found`)
+// })
