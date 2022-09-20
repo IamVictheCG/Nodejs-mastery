7d674426aa7ea28b192780ed12521d5ec04a1d80
Victor Okechukwu <VicOke360@gmail.com>
2022-09-20 09:12:18 +0100

    Public folder added

--- 2-Express-Tutorial/final/4-express-app.js
+++ 2-Express-Tutorial/final/4-express-app.js
@@ -5,7 +5,8 @@ const path = require("path")
 const PORT = 5011
 
 
-app.use(express.static('./Public'))
+// app.use(express.static('./Public'))
+
 app.get('/', (req, res) => {
     res.sendFile(path.resolve('./2-Express-Tutorial/navbar-app/index.html'))
     console.log(path)
