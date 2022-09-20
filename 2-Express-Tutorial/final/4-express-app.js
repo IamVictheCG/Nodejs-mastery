5bf7e27ee35759db79552832f555952aef5bc107
Victor Okechukwu <VicOke360@gmail.com>
2022-09-20 07:35:29 +0100

    path cleared

--- 2-Express-Tutorial/final/4-express-app.js
+++ 2-Express-Tutorial/final/4-express-app.js
@@ -1,21 +1,23 @@
 const express = require('express')
 const app = express()
-const path = require('path ')
+const path = require("path")
 
 const PORT = 5011
 
 
-app.listen(PORT, () => {
-    console.log('sever accessed')
-})
 
 app.get('/', (req, res) => {
-    // res.send("Hello")
-    res.sendFile(path.resolve(__dirname, '../navbar-app/index.html'))
-    console.log("Page up")
+    // res.sendFile(path.resolve(__dirname,'./2-Express-Tutorial/navbar-app/index.html'))
+    // console.log(path)
+    // console.log("Page up")
 })
 //  dh 
 
-app.all('#', (req, res) => {
+app.all('*', (req, res) => {
     res.status(404).send('Page not found')
 })
+
+
+    app.listen(PORT, () => {
+        console.log('sever accessed')
+    })
---
+++ app.js
@@ -0,0 +1,2 @@
+require('./2-Express-Tutorial/final/4-express-app')
+// require('./2-Express-Tutorial/navbar-app/browser-app')
