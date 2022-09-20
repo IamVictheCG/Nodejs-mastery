f0f7ebd8f0b4df3b108a84918b8427e4a68f445e
Victor Okechukwu <VicOke360@gmail.com>
2022-09-20 07:36:51 +0100

    path cleared

--- 2-Express-Tutorial/final/4-express-app.js
+++ 2-Express-Tutorial/final/4-express-app.js
@@ -8,7 +8,7 @@ const PORT = 5011
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname,'./2-Express-Tutorial/navbar-app/index.html'))
-    // console.log(path)
+    console.log(path)
     // console.log("Page up")
 })
 //  dh 
