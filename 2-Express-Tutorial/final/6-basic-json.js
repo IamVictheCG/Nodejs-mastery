695fd51ff0071591c4fd7bbf9577eec0990a1891
Victor Okechukwu <VicOke360@gmail.com>
2022-09-26 19:07:03 +0100

    6-basic-json.js updated

--- 2-Express-Tutorial/final/6-basic-json.js
+++ 2-Express-Tutorial/final/6-basic-json.js
@@ -1,2 +1,14 @@
 const express = require('express')
-const app = express
+const app = express()
+const PORT = 5011
+
+app.get('./', (req, res) => {
+    console.log("Server has been hit")
+    res.json([{name: 'Victor'}, {name: 'David'}])
+
+})
+
+
+app.listen(PORT, () => {
+    console.log('Server is running')
+})
