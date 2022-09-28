b879f8c3b2feae049d92729ef5b7de54179b1a5e
Victor Okechukwu <VicOke360@gmail.com>
2022-09-28 13:41:07 +0100

    6-basic-json.js updated

--- 2-Express-Tutorial/final/6-basic-json.js
+++ 2-Express-Tutorial/final/6-basic-json.js
@@ -1,13 +1,65 @@
 const express = require('express')
 const app = express()
-const PORT = 5011
+const PORT = 5000
 
 const {products, people} = require('../../data')
 
 app.get('/', (req, res) => {
     console.log("Server has been hit")
-    res.send('<h1><a href = "/users/product">Home Page</a></h1>')
+    res.send('<h1>Home Page</h1><p><a href =/api/products>All products</a></p><strong><a href =/api/products/1>product 1</a></strong><strong><a href =/api/products/2>product 2</a></strong><strong><a href =/api/products/3>product 3</a></strong><strong><a href =/api/products/4>product 4</a></strong>'
+)})
 
+app.get('/api/products', (req, res) => {
+    const mappedproducts = products.map((newProducts) => {
+        const {id, image, name} = newProducts
+        return {id, image, name}
+    })    
+    res.send(mappedproducts)
+})
+app.get('/api/products/1', (req, res) => {
+    const oneProduct0 = products.find((products) => products.id === 1) 
+    oneProduct0.map((newProducts) => {
+        const {id, image, name} = newProducts
+        return {id, image, name}
+    })
+    res.json(oneProduct0)
+})
+app.get('/api/products/2/:id', (req, res) => {
+    console.log(req.params)
+
+    const oneProduct = products.find((products) => products.id === 2)
+    //     () => {
+    //     const {id, image, desc} = singleProduct
+    //     return { id, image, desc }
+    // })
+    res.send(oneProduct)
+})
+app.get('/api/products/3/:id', (req, res) => {
+    const oneProduct = products.find(products.id === 3, () => {
+        const {id, image, desc} = singleProduct
+        return { id, image, desc }
+    })
+    res.json(oneProduct)
+})
+app.get('/api/products/4/:id', (req, res) => {
+        const {ProductId} = req.params
+    const oneProduct = products.find(products.id === 4, () => {
+        const {id, image, desc} = singleProduct
+        return { id, image, desc }
+    })
+    res.send(oneProduct)
+})
+
+app.get('/api/products/:id', (req, res) => {
+    const { id } = req.params
+    const oneProduct = products.find(
+        (product) => product.id === Number(id)
+        )
+    //     () => {
+    //     const {id, image, desc} = singleProduct
+    //     return { id, image, desc }
+    // })
+    res.send(oneProduct)
 })
 
 
