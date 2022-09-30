8f84a0daad70085c47e7873c489afbcec2dc15d3
Victor Okechukwu <VicOke360@gmail.com>
2022-09-30 22:28:03 +0100

    8-middleware-basic.js updated

--- authorize.js
+++ authorize.js
@@ -1,7 +1,7 @@
 const authorize = (req, res, next) => {
     const { user } = req.query
     if(user === 'victor'){
-        req.query = {name: "victor", id: 6}
+        req.user = {name: "victor", id: 6}
         console.log('Authorized')
         next()
     }
