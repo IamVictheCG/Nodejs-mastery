d24b974bb2db0a66cb2f3e8953ac6dbd7ac00e8d
Victor Okechukwu <VicOke360@gmail.com>
2022-09-30 14:03:32 +0100

    7-params-querys.js updated

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // require('./2-Express-Tutorial/final/4-express-app')
 // require('./2-Express-Tutorial/final/5-all-static')
 // require("./2-Express-Tutorial/final/6-basic-json")
-require("./2-Express-Tutorial/final/7-params-querys")
+require("./2-Express-Tutorial/final/const {products, people} = require('../../data')")
 
 
