51c70a9a951daf4d5a7195f119ffe90f2a6d2d15
Victor Okechukwu <VicOke360@gmail.com>
2022-09-30 21:32:39 +0100

    8-middleware-basic.js updated

--- 2-Express-Tutorial/final/8-middleware-basic.js
+++ 2-Express-Tutorial/final/8-middleware-basic.js
@@ -3,20 +3,7 @@ const app = express()
 // req => middleware => res
 const PORT = 1000
 
- const alldate = (req, res, next) => {
-    const method = req.method
-    const url = req.url
-    const dateD =  new Date().getDate()
-    const dateM = new Date().getMonth() + 1
-    const dateY =  new Date().getFullYear()
-    const timeH = new Date().getHours()
-    const timeM = new Date().getUTCMinutes()
-    const timeS = new Date().getUTCSeconds()
-    console.log("You are on the home page")
-    console.log(`${method}${url}---${dateD}/${dateM}/${dateY} @ ${timeH}: ${timeM}: ${timeS}`)
-    next()
-    // return
- }
+const alldate = require('../../Logger')
 
 app.get('/', alldate, (req, res) => {
     console.log("You are on the home page")
---
+++ Logger.js
@@ -0,0 +1,17 @@
+
+ const alldate = (req, res, next) => {
+    const method = req.method
+    const url = req.url
+    const dateD =  new Date().getDate()
+    const dateM = new Date().getMonth() + 1
+    const dateY =  new Date().getFullYear()
+    const timeH = new Date().getHours()
+    const timeM = new Date().getUTCMinutes()
+    const timeS = new Date().getUTCSeconds()
+    console.log("You are on the home page")
+    console.log(`${method}${url}---${dateD}/${dateM}/${dateY} @ ${timeH}: ${timeM}: ${timeS}`)
+    next()
+    // return
+ }
+
+ module.exports = (alldate)
