d677052db2d4c36d11144f94d204b7927f3c8f74
Victor Okechukwu <VicOke360@gmail.com>
2022-09-30 20:02:30 +0100

    8-middleware-basic.js added

--- 2-Express-Tutorial/final/8-middleware-basic.js
+++ 2-Express-Tutorial/final/8-middleware-basic.js
@@ -9,7 +9,7 @@ const PORT = 1000
     const dateD =  new Date().getDate()
     const dateM = new Date().getMonth() + 1
     const dateY =  new Date().getFullYear()
-    const timeH = new Date().getHours()y
+    const timeH = new Date().getHours()
     const timeM = new Date().getUTCMinutes()
     const timeS = new Date().getUTCSeconds()
     console.log("You are on the home page")
