9e6647c6eaf0f8d30d515338331da9a73d74105a
Victor Okechukwu <VicOke360@gmail.com>
2022-09-30 15:59:22 +0100

    8-middleware-basic.js added

--- 2-Express-Tutorial/final/8-middleware-basic.js
+++ 2-Express-Tutorial/final/8-middleware-basic.js
@@ -3,7 +3,22 @@ const app = express()
 // req => middleware => res
 const PORT = 1000
 
-app.get()
+app.get('/', (req, res) => {
+    const method = req.method
+    const url = req.url
+    const dateD =  new Date().getDate()
+    const dateM = new Date().getMonth() + 1
+    const dateY =  new Date().getFullYear()
+
+    console.log("You are on the home page")
+    console.log(`${method},${url},${dateD},dateM,dateY`)
+    res.send("Welcome to the Home page")
+})
+
+app.get('/about', (req, res) => {
+    console.log('You are on the about page')
+    res.send('Welcome to the about page')
+})
 
 app.listen(PORT, () => {
     console.log('Server is running')
