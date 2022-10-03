f6b3f8d3f37e2523afed90ce4ab2c7d0b9d322fa
Victor Okechukwu <VicOke360@gmail.com>
2022-10-03 06:57:50 +0100

    updates made

---
+++ 2-Express-Tutorial/final/11-methods.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const { people } = require('../../data')
+// const MP =require('../../2-Express-Tutorial/methods-public')
+
+app.use(express.static('../methods-public'))
+
+ap.get('/', (req, res) => {
+
+})
+
+app.get('/api/people', (req, res) => {
+    res.status(200).json({
+        sucess: true,
+        data: people,
+        statusCode: 200
+    })
+})
+
+const PORT = 1000
+app.listen(PORT, (req, res) => {
+    console.log("Server has been hit")
+})
