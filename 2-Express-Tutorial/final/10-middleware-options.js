eba933c3999031af1eb83520337ee42b15ca6375
Victor Okechukwu <VicOke360@gmail.com>
2022-10-03 07:00:06 +0100

    10-middleware-options.js

--- 2-Express-Tutorial/final/10-middleware-options.js
+++ 2-Express-Tutorial/final/10-middleware-options.js
@@ -1,3 +1,9 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const PORT = 1000
+const alldate = require('../../Logger')
+const authorize = require('../../authorize')
 app.use(morgan('tiny'))
 
 // app.use('/', alldate, authorize)
