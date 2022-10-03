4d4966c597a0dd2715cd5bd1178fa6579fcb893f
Victor Okechukwu <VicOke360@gmail.com>
2022-10-03 07:21:06 +0100

    updates made

--- 2-Express-Tutorial/final/11-methods.js
+++ 2-Express-Tutorial/final/11-methods.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const app = express()
 const { people } = require('../../data')
-// const MP =require('../../2-Express-Tutorial/methods-public')
+// require('../../2-Express-Tutorial/methods-public')
+// require('../methods-public')
 
-app.use(express.static('../methods-public'))
+app.use(express.static('.Public'))
 
 app.use(express.json())
 
 app.get('/', (req, res) => {
-
+// res.send()
 })
 
 app.get('/api/people', (req, res) => {
--- 2-Express-Tutorial/final/4-express-app.js
+++ 2-Express-Tutorial/final/4-express-app.js
@@ -2,18 +2,18 @@ const express = require('express')
 const app = express()
 const path = require("path")
 
-const PORT = 5011
+const PORT = 1000
 
 
 //setup static and middleware
 app.use(express.static('./Public'))
 
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve('./2-Express-Tutorial/navbar-app/index.html'))
-    console.log(path)
-    console.log("Page up")
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(path.resolve('./2-Express-Tutorial/navbar-app/index.html'))
+//     console.log(path)
+//     console.log("Page up")
+// })
 //  dh 
 
 app.all('*', (req, res) => {
