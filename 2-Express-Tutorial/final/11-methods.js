d11535e2b3ff6616a7642e4a0cf4827db8ef0142
Victor Okechukwu <VicOke360@gmail.com>
2022-10-05 13:51:30 +0100

    chore: updates made

--- 2-Express-Tutorial/final/11-methods.js
+++ 2-Express-Tutorial/final/11-methods.js
@@ -27,7 +27,7 @@ app.post('/login', (req, res) => {
     console.log(req.body)
     const { name } = req.body
     if (name) {
-        return res.status(200).send(`Hello ${name}`)
+        return res.status(200).send(`Hello ${name}`")
     }
 
     res.status(404).send('User could not be found')
